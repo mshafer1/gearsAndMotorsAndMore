8fb3522fbe67bba787412d63b153184e06dd6518
Emily Strandvold <estrand1@harding.edu>
2015-11-30 17:53:58 -0600

    Broken :(

--- gearsAndMotorsandMORE/gearsAndMotorsandMORE/gearsAndMotorsandMORE.Windows/ConstantGears.cs
+++ gearsAndMotorsandMORE/gearsAndMotorsandMORE/gearsAndMotorsandMORE.Windows/ConstantGears.cs
@@ -11,24 +11,30 @@ namespace gearsAndMotorsandMORE
 {
     class ConstantGears: IGearLibrary
     {
+        readonly string basePath = "https://doc-08-3o-docs.googleusercontent.com/docs/securesc/uorpto6d16m0opo9fv0ds926er90osb7/raqmgdoigri0o5sohu0kjji6cqi8vvlr/1448920800000/10521602918889325161/13200700494010813670/";
+
+        readonly string endPath = "?h=17437864563526662349&amp;e=view";
         public ConstantGears()
         {
-            //string path = Path.Combine(@"..\..\..\..\..\Pictures\18THex.png");
-            var path = ApplicationData.Current.LocalFolder;
-            string image = Path.Combine(path.Path, @"..\..\..\..\..\Pictures\18THex.png");
+            string image = basePath + "0BwEBnEBtHotGbTFyMk9yaFdaNHc" + endPath;
+            image = "https://doc-08-3o-docs.googleusercontent.com/docs/securesc/uorpto6d16m0opo9fv0ds926er90osb7/raqmgdoigri0o5sohu0kjji6cqi8vvlr/1448920800000/10521602918889325161/13200700494010813670/0BwEBnEBtHotGbTFyMk9yaFdaNHc?e=view&h=17437864563526662349&nonce=cc7u6anckqk86&user=13200700494010813670&hash=j8j9sard2mv6oe29qq3qo509t8eap067";
             Gear gear = new Gear(18,image,"217-3209");
             addGear(gear);
 
-            gear = new Gear(20,@"..\..\..\..\..\Pictures\20THex.png","217-2702");
+            image = basePath + "0BwEBnEBtHotGMGw0cHpIT0M3N0E" + endPath;
+            gear = new Gear(20,image,"217-2702");
             addGear(gear);
 
-            gear = new Gear(24,@"..\..\..\..\..\Pictures\24THex.png","217-2704");
+            image = basePath + "0BwEBnEBtHotGZGFxV2pnNkx1VTg" + endPath;
+            gear = new Gear(24, image,"217-2704");
             addGear(gear);
 
-            gear = new Gear(30,@"..\..\..\..\..\Pictures\30THex.png","217-2705");
+            image = basePath + "0BwEBnEBtHotGVlhfMnloRnF5MHc" + endPath;
+            gear = new Gear(30, image,"217-2705");
             addGear(gear);
 
-            gear = new Gear(36,@"..\..\..\..\..\Pictures\364THex.png","217-3214");
+            image = basePath + "0BwEBnEBtHotGc2w1aUxXQmJIc0U" + endPath;
+            gear = new Gear(36, image,"217-3214");
             addGear(gear);
         }
 
--- gearsAndMotorsandMORE/gearsAndMotorsandMORE/gearsAndMotorsandMORE.Windows/Gear.cs
+++ gearsAndMotorsandMORE/gearsAndMotorsandMORE/gearsAndMotorsandMORE.Windows/Gear.cs
@@ -5,6 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using Windows.Storage.Streams;
+using Windows.Storage;
 
 namespace gearsAndMotorsandMORE
 {
@@ -28,12 +33,33 @@ namespace gearsAndMotorsandMORE
         {
             get
             {
-                Image image = new Image();
-                BitmapImage bitmapImage = new BitmapImage();
-                Uri uri = new Uri(ImagePath);
-                bitmapImage.UriSource = uri;
-                image.Source = bitmapImage;
-                return image;
+                //Image image = new Image();
+                //BitmapImage bitmapImage = new BitmapImage();
+                //Uri uri = new Uri(ImagePath);
+                //bitmapImage.UriSource = uri;
+                //image.Source = bitmapImage;
+                //return image;
+                //var imgUrl = new Uri(ImagePath);
+                //var imageData = new HttpClient();
+
+                //// or you can download it Async won't block your UI
+                //// var imageData = await new WebClient().DownloadDataTaskAsync(imgUrl);
+
+                //var bitmapImage = new BitmapImage { CacheOption = BitmapCacheOption.OnLoad };
+                //bitmapImage.BeginInit();
+                //bitmapImage.StreamSource = new MemoryStream(imageData);
+                //bitmapImage.EndInit();
+
+                //return bitmapImage;
+                StorageFile file;
+                using (var fs = await file.OpenReadAsync())
+                using (var writer = new DataWriter(fs))
+                {
+                    writer.WriteBytes(clientResponse);
+                    await writer.StoreAsync();
+                }
+
+
             }
         }
         public string ImagePath { get; set; }
